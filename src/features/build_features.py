b461eb37826058dbfb5137ebaf224747e04f1da3
Minh Dang <minhdang@Minhs-MacBook-Pro.local>
2025-02-27 11:09:47 +0700

    better

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,34 +1,30 @@
 import numpy as np
 import pandas as pd
-
+import tensorflow as tf
 from keras.api.applications import EfficientNetB0
 from keras.api.applications.vgg19 import VGG19
 from keras.api.models import Model
-from keras.api.layers import Dense, GlobalAveragePooling2D
+from keras.api.layers import Dense, Flatten, BatchNormalization
+
 
 
 # Create EfficientNetB0 model for feature extraction
 def create_efficientnet_model():
     # Load EfficientNetB0 model with weights pre-trained on ImageNet
     efficientnet_model = EfficientNetB0(weights='imagenet', include_top=False)
-    x = GlobalAveragePooling2D()(efficientnet_model.output)
-    # Extract features from the 'global_average_pooling2d' layer, which is before the final dense layer 
-    return Model(inputs=efficientnet_model.input, outputs=x)
+    x = Flatten()(efficientnet_model.output)
+    
+    model =  Model(inputs=efficientnet_model.input, outputs=x)
+    return model
 
 # Create VGG-19 model for feature extraction
 def create_vgg_model():
     vgg_model = VGG19(weights='imagenet', include_top= False)
-    x = GlobalAveragePooling2D()(vgg_model.output)
-    return Model(inputs=vgg_model.input, outputs=x)
-
-# Add a new fully connected layer with 2 neurons for binary classification
-def add_custom_fc_layer(model):
-    x = model.output
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(1000, activation='relu')(x)  # 1000 neurons before final layer (as requested)
-    x = Dense(2, activation='softmax')(x)  # 2 neurons for binary classification
-    return Model(inputs=model.input, outputs=x)
-
+    x = Flatten()(vgg_model.output)
+    
+    model =  Model(inputs=vgg_model.input, outputs=x)
+    return model
+    
 # Function to extract features from both EfficientNetB0 and VGG-19 and fuse them
 def extract_features(df: pd.DataFrame, preprocess_images):
     # Load models
